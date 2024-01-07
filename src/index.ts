d9209d8ebbe024978e7c75102cd30e28087e88bf
biggaji <tobiajibade2017@yandex.com>
2024-01-07 16:23:48 +0100

    installed project dependecies

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { VerifiableCredential } from '@web5/credentials';
+import { Web5 } from '@web5/api';
+
+/**
+ * TODO:
+ * Issue, manage and verify VCs
+ * VC has an issuer, subject and a claim.
+ */
